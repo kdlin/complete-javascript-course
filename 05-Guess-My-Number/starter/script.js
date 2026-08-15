d22b3d8288db99be99cae659dde6327e42888f16
Kendrick Lin <kdlin@calpoly.edu>
2026-08-15 17:10:26 -0400

    state variable added for score + guess tracking

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,7 +1,11 @@
 'use strict';
 
 
+// add a rand number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20; 
 
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector(".check").addEventListener
 ('click', function() {
   const guess = Number(document.querySelector('.guess').value);
@@ -9,7 +13,18 @@ document.querySelector(".check").addEventListener
   
   if (!guess) { 
     document.querySelector('.message').textContent = 'No Number'; 
-  }
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 
+    'Correct Num!'; 
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High'; 
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    document.querySelector('.message').textContent = 'Too Low';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } 
 });
 
 
