5ee7513f0cad290ac42974f1e157fb5c95332be4
Kendrick Lin <kdlin@calpoly.edu>
2026-08-15 17:30:19 -0400

    add game over check

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,21 +10,38 @@ document.querySelector(".check").addEventListener
 ('click', function() {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess);
-  
+ 
+  // No Input
   if (!guess) { 
     document.querySelector('.message').textContent = 'No Number'; 
+
+  // When Player Wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 
-    'Correct Num!'; 
+    'Correct Num!';
+    document.querySelector('body').style.backgroundColor = '#008000';
+
+  // Guess Too high 
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High'; 
-    score--;
-    document.querySelector('.score').textContent = score;
+       if (score > 1) { 
+         document.querySelector('.message').textContent = 'Too High'; 
+        score--;
+        document.querySelector('.score').textContent = score;
+       } else {
+        document.querySelector('.score').textContent = 0;
+        document.querySelector('.message').textContent = 'game over';
+       }
+  // Too Low
   } else {
-    document.querySelector('.message').textContent = 'Too Low';
-    score--;
-    document.querySelector('.score').textContent = score;
-  } 
+       if (score > 1) { 
+         document.querySelector('.message').textContent = 'Too Low'; 
+        score--;
+        document.querySelector('.score').textContent = score;
+       } else {
+        document.querySelector('.score').textContent = 0;
+        document.querySelector('.message').textContent = 'game over';
+       }
+   }
 });
 
 
