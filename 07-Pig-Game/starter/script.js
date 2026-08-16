9e96b59130d75e505f454b6386c11bba3619f09a
Kendrick Lin <kdlin@calpoly.edu>
2026-08-16 12:53:54 -0400

    add dice roll / hide feat

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -4,9 +4,26 @@
 const score0El = document.querySelector('#score--0')
 const score1El = document.querySelector('#score--1') 
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const currScore = 0;
 
 score0El.textContent = 0;
 score1El.textContent =0; 
 diceEl.classList.add('hidden');
 
-
+// Rolling dice functionality
+btnRoll.addEventListener('click', () => {
+  const dice = Math.trunc(Math.random() * 6 + 1)
+  console.log(dice);
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+ 
+  // roll 1 ? 
+  if (dice !== 1) { 
+    // add dice to current score 
+  } else {
+    
+  }
+})
