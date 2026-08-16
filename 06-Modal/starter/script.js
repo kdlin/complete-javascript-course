c6b56bbc5456dcf86d52363bc9d75a90183218cd
Kendrick Lin <kdlin@calpoly.edu>
2026-08-15 21:00:44 -0400

    add eventListeners for modal btns and closeModal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// Modal Work
+// Select and store in a variable 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// Add EventListener to buttons
+for (let btn of btnsOpenModal) { 
+  btn.addEventListener('click', () => {
+    modal.classList.remove('hidden'); // Don't use .hidden -> hidden 
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+})
